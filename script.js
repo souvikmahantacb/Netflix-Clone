1363ab76ecb1d8ac2b1cf43f214f01567deeaed7
SOuvik <souvikmahanta9647@gmail.com>
2022-11-12 23:02:27 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const qns= document.getElementById("qns");
+const ans= document.getElementById("ans");
+ans.style.display ="none"
+qns.addEventListener("click", (event)
+=>{
+    if(ans.style.display == "none"){
+        ans.style.display == "block"
+    }
+    else{
+    ans.style.display == "none"
+    }
+})
